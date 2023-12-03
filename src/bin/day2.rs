eeb3459878dc6ddcb70884a3954332cd733969f6
Francisco Almeida <nyliki.2003@gmail.com>
2023-12-03 15:47:31 +0000

    Day2 done

--- src/bin/day2.rs
+++ src/bin/day2.rs
@@ -1,39 +1,48 @@
-use std::u32;
-
 use advent_of_code_2023::read_file::read_file;
 
 
 
 
-fn is_game_valid(line: String, game_config: &Vec<(u32, &str)>) -> u32 {
+fn is_game_valid(line: String) -> Vec<u32> {
     let binding = line.split(&[':', ' ', ',', ';'][..]).collect::<Vec<&str>>();
     let mut it = binding.into_iter();
-    let original_iter = it.clone();
+    //red, green, blue
+    let mut res = vec![0, 0, 0];
+    let mut _idx = 0;
 
     
-    let mut id = 0;
     let mut parse_num;
     let mut num;
     let mut color;
-    for conf in game_config {
-        while let Some(word) = it.next(){
-            match word {
-                "Game" => id = it.next().unwrap().parse::<u32>().unwrap(),
-                x => {
-                    parse_num = x.parse::<u32>();
-                    if !parse_num.is_err() {
-                        color = it.next().unwrap();
-                        num = parse_num.unwrap();
-                        if conf.1 == color && conf.0 < num {
-                            return 0
-                        }
+    
+    while let Some(word) = it.next(){
+        match word {
+            "Game" => {it.next(); it.next();},
+            x => {
+                parse_num = x.parse::<u32>();
+                if !parse_num.is_err() {
+                    color = it.next().unwrap();
+                    num = parse_num.unwrap();
+                    match color {
+                        "red" => _idx = 0,
+                        "green" => _idx = 1,
+                        "blue" => _idx = 2,
+                        _ => panic!("Wrong color")
                     }
+                    if res[_idx] < num {res[_idx] = num}
                 }
             }
         }
-        it = original_iter.clone().into_iter();
     }
-    id
+    res
+}
+
+fn sum_powers(powers: Vec<u32>) -> u32{
+    let mut sum = 1;
+    for p in powers {
+        sum *= p;
+    }
+    sum
 }
 
 
@@ -42,12 +51,11 @@ fn is_game_valid(line: String, game_config: &Vec<(u32, &str)>) -> u32 {
 
 fn main() {
     let lines = read_file("input/day2.txt");
-    let game_config: Vec<(u32, &str)> = vec![(12, "red"), (13, "green"), (14, "blue")];
 
     let mut sum = 0;
     let mut i = 1;
     for line in lines {
-        sum += is_game_valid(line, &game_config);
+        sum += sum_powers(is_game_valid(line));
         println!("Game {}: {}", i, sum);
         i +=1;
     }
